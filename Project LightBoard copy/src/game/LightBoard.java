d05cbd8d420ed668e4b6f85ee90fc6ac58c11fd2
Ivanca19 <105860596+Ivanca19@users.noreply.github.com>
2022-06-01 15:06:30 -0400

    Update LightBoard.java

--- Project LightBoard copy/src/game/LightBoard.java
+++ Project LightBoard copy/src/game/LightBoard.java
@@ -1,5 +1,6 @@
 package game;
 
+// This class creates a LightBoard object which is a double boolean array used to represent lights that are on or off.
 public class LightBoard {
 
     private boolean[][] _board;
@@ -11,6 +12,7 @@ public class LightBoard {
 
     public boolean[][] getBoard() { return _board;}
 
+    // changes all of the booleans in _board to false
     public void turnOff() {
         for(int r = 0; r < _board.length; r++) {
             for (int c = 0; c < _board[r].length; c++) {
@@ -19,6 +21,7 @@ public class LightBoard {
         }
     }
 
+    // changes all of the booleans in _board to true
     public void turnOn() {
         for(int r = 0; r < _board.length; r++) {
             for (int c = 0; c < _board[r].length; c++) {
@@ -27,6 +30,7 @@ public class LightBoard {
         }
     }
 
+    // changes all of the booleans in a specific row of _board to false
     public void turnRowOff(int row) {
 
         for(int r = row; r < row + 1; r++) {
@@ -35,7 +39,8 @@ public class LightBoard {
             }
         }
     }
-
+    
+    // changes all of the booleans in a specific row of _board to true
     public void turnRowOn(int row) {
         for(int r = row; r < row + 1; r++) {
             for (int c = 0; c < _board[r].length; c++) {
@@ -44,18 +49,21 @@ public class LightBoard {
         }
     }
 
+    // changes all of the booleans in a specific col of _board to false
     public void turnColOff(int col) {
         for(int r = 0; r < _board.length; r++) {
             _board[r][col] = false;
         }
     }
 
+    // changes all of the booleans in a specific col of _board to true
     public void turnColOn(int col) {
         for(int r = 0; r < _board.length; r++) {
             _board[r][col] = true;
         }
     }
 
+    // changes all of the booleans in a specific grid of _board to false
     public void turnGridOff(int sRow, int sCol, int eRow, int eCol) {
         for(int r = sRow; r <= eRow; r++) {
             for (int c = sCol; c <= eCol; c++) {
@@ -64,6 +72,7 @@ public class LightBoard {
         }
     }
 
+    // changes all of the booleans in a specific grid of _board to true
     public void turnGridOn(int sRow, int sCol, int eRow, int eCol) {
         for(int r = sRow; r <= eRow; r++) {
             for (int c = sCol; c <= eCol; c++) {
@@ -72,6 +81,7 @@ public class LightBoard {
         }
     }
 
+    // changes all of the booleans in _board randomly to true or false
     public void randomize() {
         for(int r = 0; r < _board.length; r++) {
             for (int c = 0; c < _board[r].length; c++) {
